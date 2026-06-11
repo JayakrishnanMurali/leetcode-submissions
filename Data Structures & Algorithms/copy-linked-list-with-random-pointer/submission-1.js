53322332ddabe95a4b52d8241c08e4d069df8fe3
jay <jayakrishnan1012@gmail.com>
2026-06-11 06:44:10 +0530

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.js
@@ -0,0 +1,38 @@
+// class Node {
+//   constructor(val, next = null, random = null) {
+//       this.val = val;
+//       this.next = next;
+//       this.random = random;
+//   }
+// }
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+    copyRandomList(head) {
+        if(!head) return null;
+
+        const map = new Map()
+
+        let curr = head;
+        while(curr){
+            map.set(curr, new Node(curr.val))
+            curr = curr.next;
+        }
+
+        curr = head;
+        while(curr){
+            const clone = map.get(curr)
+
+            clone.next = curr.next ? map.get(curr.next) : null;
+            clone.random = curr.random ? map.get(curr.random) : null
+
+            curr = curr.next;
+        }
+
+        return map.get(head)
+
+    }
+}
