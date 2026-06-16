010c94b70b7f07150fbc8f19381052b6a505b1e5
jay <jayakrishnan1012@gmail.com>
2026-06-16 21:13:37 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n) {
+        const res = []
+        const path = []
+
+        function backtrack(open, close){
+            if(path.length === 2*n){
+                res.push(path.join(""))
+                return
+            }
+
+            if(open < n){
+                path.push("(")
+                backtrack(open + 1, close)
+                path.pop()
+            }
+
+            if(close < open){
+                path.push(")")
+                backtrack(open, close + 1)
+                path.pop()
+            }
+
+        }
+
+        backtrack(0, 0)
+        return res;
+    }
+}
