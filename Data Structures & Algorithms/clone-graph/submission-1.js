255c3125fbb7e2ab100fb308df67b01648b2d11b
jay <jayakrishnan1012@gmail.com>
2026-06-19 17:03:00 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.js
@@ -0,0 +1,39 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        if(node === null) return null
+
+        const map = new Map()
+        const root = node;
+
+        function dfs(nd){
+            if(map.has(nd)) return map.get(nd);
+
+            const clone = new Node(nd.val)
+            map.set(nd, clone)
+
+            for(const neigh of nd.neighbors){
+                clone.neighbors.push(dfs(neigh))
+            }
+
+            return clone
+        }
+
+        dfs(node)
+
+        return map.get(root)
+    }
+}
