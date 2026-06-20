314bfa4eeb6615ecbbcf8124c5f83c9be03ef112
jay <jayakrishnan1012@gmail.com>
2026-06-20 16:31:05 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const adj = Array.from({length: numCourses}, () => [])
+        const indegree = new Array(numCourses).fill(0)
+
+        for(const [a,b] of prerequisites){
+            adj[b].push(a)
+            indegree[a]++
+        }
+
+        const queue = [];
+        for (let i = 0; i < numCourses; i++) {
+            if (indegree[i] === 0) queue.push(i);
+        }
+
+        let processed = 0;
+
+        while (queue.length) {
+            const node = queue.shift();
+
+            processed++;
+            for (const next of adj[node]) {
+                if (--indegree[next] === 0) queue.push(next);
+            }
+        }
+
+        
+        
+
+        return processed === numCourses;
+    }
+}
