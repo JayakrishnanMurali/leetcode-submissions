12d756ecf12117ba69e446c6486d15a30ee4b98e
jay <jayakrishnan1012@gmail.com>
2026-06-20 16:49:05 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        if(edges.length !== n-1) return false
+
+        const parent = Array.from({length: n}, (_,i) => i)
+
+        const find = (x) => {
+            while(parent[x] != x){
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            }
+
+            return x
+        }
+
+        for(const [a,b] of edges){
+            const ra = find(a)
+            const rb = find(b)
+
+            if(ra === rb) return false
+            parent[ra] = rb;
+        }
+
+        return true;
+    }
+}
