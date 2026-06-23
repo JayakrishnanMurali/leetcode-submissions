e5990264e208d922f585e5c30b620b721f7e0d2b
jay <jayakrishnan1012@gmail.com>
2026-06-23 22:29:47 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        const memo = new Map()
+
+        function dfs(i){
+            if(i === n) return 1;
+            if(i > n) return 0;
+
+            if(memo.has(i)) return memo.get(i)
+
+            const ways = dfs(i + 1) + dfs(i + 2)
+            memo.set(i, ways)
+
+            return ways
+        }
+
+        return dfs(0)
+    }
+}
